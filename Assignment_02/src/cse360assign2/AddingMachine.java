8499f1432d29303820cd0b619a3dfdeb105ce5e1
daniel <daniel@172.31.98.218>
2019-06-17 09:40:52 -0700

    this is the initial version

---
+++ Assignment_02/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,34 @@
+/**Name: Daniel Martinez
+ *Assignment #: 2
+ *Description: this assignment is a calculator
+ */
+package cse360assign2;
+
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {//this is going to be getting the total
+		return 0;
+	}
+	
+	public void add (int value) {//this is the add function
+		
+	}
+	
+	public void subtract (int value) {//this is the subtract function
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
